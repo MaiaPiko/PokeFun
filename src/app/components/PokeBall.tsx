88880c4c157e19f50942941695800a08fc0d2fe6
MaiaPiko <maia.mlynczak@gmail.com>
2023-08-25 22:24:28 +0200

    Don't get attached

--- src/app/components/PokeBall.tsx
+++ src/app/components/PokeBall.tsx
@@ -5,66 +5,61 @@ import pika from "../../../public/pika.svg";
 import Image from "next/image";
 import { useState } from "react";
 export default function PokeBall({
-	
 	link,
 	color,
+	isHover
 }: {
-	
 	link: string;
 	color: string;
+	isHover: boolean;
 }) {
-	const [isHover, setIsHover] = useState(false);
+	// const [isHover, setIsHover] = useState(false);
 
-	const handleMouseEnter = () => {
-		setIsHover(true);
-	};
-	const handleMouseLeave = () => {
-		setIsHover(false);
-	};
+	// const handleMouseEnter = () => {
+	// 	setIsHover(true);
+	// };
+	// const handleMouseLeave = () => {
+	// 	setIsHover(false);
+	// };
 
-// 	const [pokeColor, setPokeColor] = useState("")
-// useEffect(()=>{
-// 	if (color == "bluePoke"){
-// 		setPokeColor("#0000a3")
-// 	}
-// 	else if (color=="redPoke"){
-// 		setPokeColor('#ed1e25')
-// 	}
-// 	else if (color=="pokeYellow"){
-// 		setPokeColor('#ffbf00')
-// 	}
-// },[])
 	return (
 		<>
 			<Link href={link}>
 				<div
-					className=" cursor-pointer "
+					className={`cursor-pointer   ${isHover? "relative z-50 justify-center items-center screen":"absolute z-0"} `}
 					id="pokeball"
-					onMouseEnter={handleMouseEnter}
-					onMouseLeave={handleMouseLeave}
+					// onMouseEnter={handleMouseEnter}
+					// onMouseLeave={handleMouseLeave}
 				>
-					{/* <h1 className="-translate-y-10 text-4xl font-bold  drop-shadow-sm text-center">
-						<span >{textPart1}</span><span className={`text-${color}`}>{textPart2}</span>
-					</h1> */}
-					<div className="relative w-80 h-80 mx-auto" id="container">
+					
+					<div
+						className={` ${
+							isHover ? "w-80 h-80" : "w-40 h-40"
+						} mx-auto`}
+						id="container"
+					>
 						<div
-							className={`absolute top-0 left-0 w-full h-40 rounded-t-full border-8 border-black`}
+							className={`absolute top-0 left-0 ${isHover?" w-full h-40 border-8": " border-4 w-full h-20"} rounded-t-full  border-black`}
 							style={{
-								backgroundColor:`${color}`,
+								backgroundColor: `${color}`,
 								transform: isHover ? "translateY(-10px)" : "translateY(0)",
 								transition: "transform 0.3s ease-in-out",
-								
 							}}
 							id="red"
 						></div>
 
-						{/* <div className="absolute top-0 left-0 w-full h-40 bg-red-600 rounded-t-full border-8 border-black transform hover:-translate-y-5 transition-transform" id="red"></div> */}
-						<div className="absolute bottom-0 left-0 w-full h-40 bg-white rounded-b-full border-8 border-black"></div>
 						<div
-							className="absolute bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-28 flex items-center justify-center w-20 h-20 bg-white rounded-full border-8 border-black"
+							className={`absolute bottom-0 left-0 w-full ${
+								isHover ? "h-40 border-8": " border-4 h-20"
+							} bg-white rounded-b-full  border-black`}
+						></div>
+						<div
+							className={`absolute bottom-0 left-1/2 transform -translate-x-1/2 ${isHover?"-translate-y-28":"-translate-y-14"} flex items-center justify-center ${
+								isHover ? "w-20 h-20 border-8": " border-4 w-10 h-10"
+							} bg-white rounded-full  border-black`}
 							id="white"
 						></div>
-					</div>
+						</div>
 				</div>
 			</Link>
 		</>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+"use client";
 import Head from "next/head";
 import Image from "next/image";
 import { Metadata } from "next";
 import Box from "./components/PokeBall";
 import PokeBall from "./components/PokeBall";
+
 import RandomPokemonSprite from "./components/RandomPokeSprite";
 import LoadingPokeBall from "./components/LoadingPokeBall";
+import { useState } from "react";
+
 export const metadata: Metadata = {
 	title: "Pokefun",
 	description: "Pokemon fun time",
@@ -14,25 +18,86 @@ export default function Home() {
 	const pokeBlue = "#0000a3";
 	const pokeRed = "#ed1e25";
 	const pokeYellow = "#ffbf00";
+	const [hoverId, setHoverId] = useState<string>("0");
+	
+	const [isHover, setIsHover] = useState(false)
+	const handleMouseEnter = (id: string) => {
+		setHoverId(id);
+	
+
+	};
+
+	const handleMouseLeave = (id: string) => {
+		setHoverId("0");
+	
+
+	};
+
+
+
 	return (
 		<>
-			<main>				
-        <div className="flex justify-center items-center h-2/3 mt-20 ">
+			<main>
+				<div className={`flex justify-center h-2/3 mt-20 `}>
+					
+					<div className={`flex content-center ${hoverId=="0"?"gap-44":"gap-0"}`}>
+					<div
+						// className={`
+						// ${hoverId=="2"|| hoverId=="3" ? "transition ease-in delay-7 translate-x-20 skew-1":
+						//  hoverId=="1"? "transition ease-in delay-70 translate-x-20 z-50":""}`}
+						id="1"
+						onMouseEnter={() => handleMouseEnter("1")}
+						onMouseLeave={() => handleMouseLeave("1")}
+					>
+						<PokeBall link={"PokeData/1"} color={pokeBlue}  isHover={hoverId == "1"? true:false}/>
+						<h1
+							className={`font-semibold text-lg ${
+								hoverId == "1" ? "flex" : "hidden"
+							}`}
+						>
+							Pokedex
+						</h1>
+					</div>
+					<div
+						className={`${hoverId=="1" ? " -translate-x-40 z-0":""}`}
+						id="2"
+						onMouseEnter={() => handleMouseEnter("2")}
+						onMouseLeave={() => handleMouseLeave("2")}
+					>
+						<PokeBall link={"#"} color={pokeRed} isHover={hoverId == "2"? true:false}/>
+						<h1
+							className={`font-semibold text-lg ${
+								hoverId == "2" ? "flex" : "hidden"
+							}`}
+						>
+							Pokewhich
+						</h1>
+					</div>
+					
+					{/* <div
+						id="3"
+						onMouseEnter={() => handleMouseEnter("3")}
+						onMouseLeave={() => handleMouseLeave("3")}
+						className={`${
+							hoverId === "2"
+							  ? "transition ease-in delay-7 -translate-x-20 z-0"
+							  : hoverId == "1"
+							  ? "transition ease-in delay-7 -translate-x-20 z-0"
+							  : ""
+						  }`}
+						  
 
-				<div className="flex justify-center items-center  mt-20 mx-10 space-y-0">
-					<div className="flex flex-col justify-center items-center h-80 space-y-0">
-						<PokeBall link={"PokeData/1"} color={pokeBlue} />
-						<h1 className="font-semibold text-lg ">Pokedex</h1>
-					</div>
-					<div className="flex flex-col justify-center items-center  space-y-0">
-						<PokeBall link={"#"} color={pokeRed} />
-						<h1 className="font-semibold text-lg ">Pokewho</h1>
-					</div>
-					<div className="flex flex-col justify-center items-center  space-y-0">
-						<PokeBall link={"#"} color={pokeYellow} />
-						<h1 className="font-semibold text-lg ">Pokewhat</h1>
-					</div>
-          </div>
+					>
+						<PokeBall link={"#"} color={pokeYellow} isHover={hoverId == "3"? true:false}/>
+						<h1
+							className={`font-semibold text-lg ${
+								hoverId == "3" ? "flex" : "hidden"
+							}`}
+						>
+							Pokewhat
+						</h1>*/}
+					</div> 
+					
 				</div>
 			</main>
 		</>
