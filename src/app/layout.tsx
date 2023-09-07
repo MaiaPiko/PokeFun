bb7f82e277844ba97ed787a75e886985ed07d814
MaiaPiko <maia.mlynczak@gmail.com>
2023-09-07 13:38:44 +0200

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,15 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from './components/Navbar'
+import type { Metadata } from 'next'
+
 const inter = Inter({ subsets: ['latin'] })
 
+
+export const metadata: Metadata = {
+  title: 'Pokefun',
+  description: 'The ultimate Pokemon quiz application!',
+}
 
 
 export default function RootLayout({
