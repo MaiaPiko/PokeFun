e92eb0e693797938294bc1eec6504c49e6742cef
MaiaPiko <maia.mlynczak@gmail.com>
2023-09-25 18:11:09 +0200

    Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from './components/Navbar'
 import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar/>
         {children}
+        <Analytics/>
         </body>
     </html>
   )
